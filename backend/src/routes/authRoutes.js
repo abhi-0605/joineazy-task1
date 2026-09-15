e9f48fa9e1c03f07b86cd9738396fb850b120afd
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-09-16 00:09:05 +0530

    fix path of auth constroller

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {register,login,me}=require('../middleware/authController');
+const {register,login,me}=require('../controllers/authController');
 const {authenticate,requireRole}=require('../middleware/auth');
 
 
